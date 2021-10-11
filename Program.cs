b0f2229947e5f42bcf46b7ba67e90696897deef1
umme <unnipa01@yahoo.com>
2021-10-11 13:51:55 -0500

    initial copy

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Net.Http;
+using Newtonsoft.Json.Linq;
+
+namespace findWeather
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HttpClient client = new HttpClient();
+            var checkedWeather = new checkWeather(client);
+            Console.WriteLine();
+            var weather = checkedWeather.openWeather();
+            Console.WriteLine($"Todays weather is  {weather}");
+        }
+    }
+}
---
+++ checkWeather.cs
@@ -0,0 +1,32 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace findWeather
+{
+    public class checkWeather
+    {
+        private HttpClient aClient;
+
+        public checkWeather(HttpClient  client)
+        {
+            aClient = client;
+        }
+
+        public string openWeather()
+        {
+            Console.WriteLine($"Please enter the api key to find weather information");
+            var apiKey = Console.ReadLine();
+            Console.WriteLine("Please enter the name of the city");
+            var cityName = Console.ReadLine();
+            var weatherURL = $"https://api.openweathermap.org/data/2.5/weather?q={cityName}&units=imperial&appid={apiKey}";
+            var weatherResponse = aClient.GetStringAsync(weatherURL).Result;
+            var weatherObj = JObject.Parse(weatherResponse).GetValue("main").ToString();
+            return weatherObj;
+        }
+    }
+}
